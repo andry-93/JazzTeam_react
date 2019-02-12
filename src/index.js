13b93689a6dbf8f0ccc8a558711e7439dd1e6743
Andrei <andry-93@bk.ru>
2019-02-12 22:47:07 +0200

    favicon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 import App from './components/App';
 
+import './img/favicons/android-icon-192x192.png';
+import './img/favicons/apple-icon-120x120.png';
+import './img/favicons/ms-icon-144x144.png';
+import './img/favicons/favicon-32x32.png';
+
 const store = createStore(rootReducer);
 
 // Render the main component into the dom
